3d498ed2c50e74776e35799d7bf8053934f1490e
Aleksey Batnin <al-bat-ros@mail.ru>
2019-10-23 09:09:47 +0500

    if

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let lang = 'en';
+let ruWeek = 'понедельник, вторник, среда, четверг, пятница, суббота, воскресение.';
+let enWeek = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.';
+// Через if 
+if (lang === 'ru'){
+   console.log(ruWeek);
+} else if (lang === 'en'){
+    console.log(enWeek);
+}
+
+
