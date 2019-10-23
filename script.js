4008b920e37ff7a990a7b7cea275423b536f29ae
Aleksey Batnin <al-bat-ros@mail.ru>
2019-10-23 23:13:00 +0500

    exercise 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let lang = 'en';
+let lang = 'ru';
 let ruWeek = 'понедельник, вторник, среда, четверг, пятница, суббота, воскресение.';
 let enWeek = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.';
 // Через if 
@@ -9,5 +9,17 @@ if (lang === 'ru'){
 } else if (lang === 'en'){
     console.log(enWeek);
 }
+// Через switch-case
+switch (lang){
+    case 'ru':
+        console.log(ruWeek);
+        break;
+    case 'en':
+        console.log(enWeek);
+        break;
+}
+// Через многомерный массив
 
+ let arr = {'ru': ruWeek.split(), 'en': enWeek.split()};
+console.log(arr[lang].join());
 
